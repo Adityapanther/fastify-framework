8214b2b50ce268053664a20e3d3fad9ef200a153
raj68518 <raj68518@gmail.com>
2021-03-16 13:46:11 +0530

    mongodb added and implimented

--- routes/firstRoutes.js
+++ routes/firstRoutes.js
@@ -1,8 +1,23 @@
-async function routes(app, options){
+async function hell(app, options) {
+    const collection = await app.mongo.db.collection('name');
 
-    app.get("/user", (req, rep)=>{
-        return "he all she";
+
+    app.get('/', async (req, res) => {
+        return {"greeting":"hello"};
+    })
+
+    app.get("/user", async (req, rep) =>{
+        return {"user":"he all she"};
+    })
+
+    app.get('/name', async (req, rep)=>{
+        var na = await collection.find().toArray();
+        if(na.length === 0){
+            throw new Error("no document found")
+        }else{
+            return na[0]
+        }
     })
 }
 
-module.exports = routes;
+module.exports = hell;
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-var fastify = require('fastify');
-var app = fastify({logger: true});
+const fastify = require('fastify');
+const app = fastify({logger: true});
 
 // register routes
+const dbConnector = require("./routes/db/dbConnector");
+const rou = require("./routes/firstRoutes");
 
-app.register(require('./routes/firstRoutes'));
+app.register(dbConnector);
+app.register(rou);
 
-app.get('/', (req, res) =>{
-    return "hello";
-})
 
-app.listen("3000", (err, addr)=>{
-    if(err) throw "server not running";
-    app.log.info(`server running on ${addr}`);
-})
+const start = async function(){
+    try{
+       await app.listen("3000")
+    }catch(e){
+        app.log.error(e);
+    }
+}
+
+start();
